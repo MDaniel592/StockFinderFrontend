7c28f530c39b7b4bb630f813489f609806e7c8f1
MDaniel <mmdaniel@protonmail.com>
2023-09-24 22:48:40 +0000

    minor adjustment

--- components/Layouts/header/AuthSection.jsx
+++ components/Layouts/header/AuthSection.jsx
@@ -11,8 +11,8 @@ export default function AuthSection({ userData }) {
 
   const onLogoutButtonPressed = e => {
     e.preventDefault()
-    setUserData(undefined)
     authService.logout()
+    setUserData(undefined)
     router.push('/')
   }
 
@@ -21,7 +21,7 @@ export default function AuthSection({ userData }) {
       <div className="z-20 hidden md:flex items-center justify-end md:flex-1 lg:w-0 gap-4">
         <Link
           href={userData ? '/profile' : '/login'}
-          className="text-base font-semibold text-white hover:text-blue-500"
+          className="text-base font-semibold btn-transparent-white"
         >
           {userData ? 'Panel de usuario' : 'Iniciar Sesion'}
         </Link>
@@ -29,7 +29,7 @@ export default function AuthSection({ userData }) {
         {userData && (
           <button
             onClick={onLogoutButtonPressed}
-            className="mt-2 inline-flex justify-center px-2 py-2 rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-800"
+            className="inline-flex justify-center btn-ok"
           >
             Cerrar Sesión
           </button>
